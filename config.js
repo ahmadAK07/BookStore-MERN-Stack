157cb89a46730b076ef05bea2369fe57f3f2a4c5
ahmadAK07 <156776092+ahmadAK07@users.noreply.github.com>
2024-02-26 16:22:24 +0500

    Add backend files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+export const PORT = 3000;
+
+//username = muhammadahmadamin512 password = NuqwfSrPDp44iDUf
+
+export const mongoDBURL  = "mongodb+srv://muhammadahmadamin512:NuqwfSrPDp44iDUf@book-store-mern.kgjgihs.mongodb.net/books-collection?retryWrites=true&w=majority";
